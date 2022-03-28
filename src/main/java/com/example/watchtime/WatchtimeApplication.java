bbb845d6da96ec9a310ce2f89822a8718042410e
whokegard <rewelox@hotmail.com>
2022-03-28 11:17:09 +0200

    Initial commit

---
+++ src/main/java/com/example/watchtime/WatchtimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.watchtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchtimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchtimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/watchtime/WatchtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.watchtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchtimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
