f1407b1cfe75adfa8da46f0066e65aaed607a2df
whokegard <rewelox@hotmail.com>
2022-03-30 13:45:17 +0200

    Updated TVShow classes.

--- src/main/java/com/example/watchtime/api/TVShowController.java
+++ src/main/java/com/example/watchtime/api/TVShowController.java
@@ -1,6 +1,7 @@
 package com.example.watchtime.api;
 
 import com.example.watchtime.model.TVShow;
+import com.example.watchtime.service.MemberService;
 import com.example.watchtime.service.TVShowService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,17 @@ public class TVShowController {
     }
 
     @GetMapping("/tvshow/{id}")
-    public TVShow getTVShowById(@PathVariable("id") Long id) {
+    public TVShow getTVShowById(@PathVariable("id") long id) {
         return tvShowService.getTVShowById(id);
+    }
+
+    @GetMapping("/members/{member_id}/tvshow/{tvshow_id}")
+    public void deleteTVShowByID(@PathVariable("member_id") long member_id, @PathVariable("id") long tvshow_id) {
+        tvShowService.deleteTVShowById(member_id, tvshow_id);
+    }
+
+    @GetMapping("/members/{id}/tvshow")
+    public void deleteAllTVShows() {
+        tvShowService.deleteAllTVShows();
     }
 }
