faade88abfba3cbaf1ef6d139c1223fa53496b76
b3nni <benjamin_nilsson@hotmail.com>
2022-03-30 14:59:19 +0200

    Frontend: added get a member by Id method.

--- src/frontend/src/client.js
+++ src/frontend/src/client.js
@@ -1,1 +1,15 @@
 import fetch from 'unfetch';
+
+const checkStatus = response => {
+    if (response.ok) {
+        return response;
+    }
+
+    const error = new Error(response.statusText);
+    error.response = response;
+    return Promise.reject(error);
+}
+
+export const getAMember = () =>
+    fetch("api/members/13")
+        .then(checkStatus);
